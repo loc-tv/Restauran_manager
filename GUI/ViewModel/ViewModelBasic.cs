04d364261f671dc4dbc60b121fa79ced7397935c
Trần Lộc <loc1852002thpt@gmail.com>
2022-11-19 11:52:55 +0700

    Update 19/11/22

---
+++ GUI/ViewModel/ViewModelBasic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace GUI.ViewModel
+{
+    public abstract class ViewModelBasic : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

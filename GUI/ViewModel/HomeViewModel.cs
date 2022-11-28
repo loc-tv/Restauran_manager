1defa00a360d2ad12e62b1186507c03ac5b33108
Trần Lộc <loc1852002thpt@gmail.com>
2022-11-29 00:09:58 +0700

    Update 29/11/2022

--- GUI/ViewModel/HomeViewModel.cs
+++ GUI/ViewModel/HomeViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using FontAwesome.Sharp;
 using GUI.Model;
 using GUI.Repositories;
 
@@ -14,7 +16,13 @@ namespace GUI.ViewModel
     {
         //Fields
         private UserModelAccout _currentUserAccout;
+        private ViewModelBasic _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
         private IUserRepository userRepository;
+
+        //Properties
         public UserModelAccout CurrentUserAccout
         {
             get
@@ -27,11 +35,74 @@ namespace GUI.ViewModel
                 OnPropertyChanged(nameof(CurrentUserAccout));
             }
         }
+        public ViewModelBasic CurrentChildView 
+        { 
+            get
+            {
+                return _currentChildView;
+            }
+            set 
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            } 
+        }
+        public string Caption 
+        {
+            get 
+            {
+                return _caption;
+            }
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            } 
+        }
+        public IconChar Icon 
+        { 
+            get 
+            {
+                return _icon; 
+            }
+            set 
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon)); 
+            }
+        }
+
+        //Commands
+        public ICommand ShowPageHomeViewCommand { get; }
+        public ICommand ShowPageListViewCommand { get; }
+
         public HomeViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccout = new UserModelAccout();
+
+            //Initialize commands
+            ShowPageHomeViewCommand = new ViewModelCommand(ExecuteShowPageHomeViewCommand);
+            ShowPageListViewCommand = new ViewModelCommand(ExecuteShowPageListViewCommand);
+
+            //Default view
+            ExecuteShowPageHomeViewCommand(null);
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowPageListViewCommand(object obj)
+        {
+            CurrentChildView = new PageListViewModel();
+            Caption = "List";
+            Icon = IconChar.List;
+        }
+
+        private void ExecuteShowPageHomeViewCommand(object obj)
+        {
+            CurrentChildView = new PageHomeViewModel();
+            Caption = "Home";
+            Icon = IconChar.Home;
+
         }
 
         private void LoadCurrentUserData()
